43369274d99026c39b8000819cc49892c0d5ee27
Oliver Olsen <oliver@skou-olsen.dk>
2024-08-20 14:13:12 +0200

    Viterbi "skeleton" start

--- viterbi-111_101.py
+++ viterbi-111_101.py
@@ -6,12 +6,16 @@ import numpy as np
 
 
 
+#Returns length of sequence (used for trellis diagram)
 def sequence(seq: list) -> int:
     seq_elements = len(seq)
     return seq_elements
 
 
-
+def trellis_construct(matrix_size: int, generator_states: list)-> list:
+    # Twice the length of the generator, since each point can be accessed from two points
+    trellis_hamming = np.zeros((2*len(generator_states), matrix_size))
+    return trellis_hamming
 
 
 
@@ -22,7 +26,18 @@ def sequence(seq: list) -> int:
 
 if __name__ == "__main__":
 
-    # Sequence to be decoded. Entered as a list of strings
-    input_sequence = [str("11"), str("01"), str("01"), str("10"), str("01")]
+    # Generator polynomial
+    generator111_101 = [["00", "11"], ["10", "01"], ["11", "00"], ["01", "10"]]
 
-    length_of_sequence = (input_sequence)
+
+
+
+    # Sequence to be decoded. Entered as a list of strings
+    input_sequence = ["11", "01", "01", "10", "01"]
+
+
+    # Get length of sequence
+    length_of_sequence = sequence(input_sequence)
+
+    # Create matrix. Uses the length of the sequence to determine size
+    print(trellis_construct(length_of_sequence, generator111_101))
