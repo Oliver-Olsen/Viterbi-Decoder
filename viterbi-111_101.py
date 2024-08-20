71fe8d8e605bab81a15cfac6ab5c2191fe81e0f1
Oliver Olsen <oliver@skou-olsen.dk>
2024-08-20 13:04:59 +0200

    Projekt Commit test

--- viterbi-111_101.py
+++ viterbi-111_101.py
@@ -1,2 +1,28 @@
-#Git Test
+#Oliver Skou Olsen
+#Eksamens Projekt - Viterbi Dekoder
+#34220 Coding in Communication Systems
+#Kommentarer skrives på engelsk
 import numpy as np
+
+
+
+def sequence(seq: list) -> int:
+    seq_elements = len(seq)
+    return seq_elements
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    # Sequence to be decoded. Entered as a list of strings
+    input_sequence = [str("11"), str("01"), str("01"), str("10"), str("01")]
+
+    length_of_sequence = (input_sequence)
